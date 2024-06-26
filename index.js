6253c81a62537cb0dd5562e3da6054a051342765
sakshamgupta930 <guptasaksham3071@gmail.com>
2024-06-26 10:54:36 +0530

    Ivr commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ivr = require("./routes/ivr.js")
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/ivr', ivr);
